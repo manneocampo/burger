779aff1ae9596c0f1da8d1045107e38c9a63b8c8
Michelle <manneocampo@gmail.com>
2017-12-05 17:13:44 -0500

    added all first code to burger.js and added delete to orm.js

--- config/orm.js
+++ config/orm.js
@@ -77,6 +77,18 @@ var orm = {
 			}
 			cb(results);
 		});
+	}, 
+	delete: function(table, condition, cb){
+		var queryString = 'DELETE FROM' + table;
+		queryString += 'WHERE';
+		queryString += condition;
+
+		connection.query(queryString, function(err, result){
+			if(err){
+				throw err;
+			}
+			cb(results);
+		});
 	}
 };
 
