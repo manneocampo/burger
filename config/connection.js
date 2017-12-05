b41127ee45c87cd2ac1f81bddb6e5888d5a53c1e
Michelle <manneocampo@gmail.com>
2017-12-04 21:59:16 -0500

    added starter code

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,27 @@
 // Inside the connection.js file, setup the code to connect Node to MySQL.
 // Export the connection.
+
+var mysql = require('mysql');
+
+
+//declare config for our db
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'H8passwords1!',
+	database: 'burgers_db'
+}); 
+
+//connect to database 
+connection.connect(function(err){
+	if(err){
+		console.log("error connecting:" + err.stack);
+		return;
+	}
+	console.log("connected as id" + connection.threadId);
+});
+
+module.exports = connection;
+
+
+
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,12 @@
 // Express
 // burger.js
 // Create the router for the app, and export the router at the end of your file.
+
+var express = require('express');
+
+var router = express.Router();
+
+var burger = require('../models/burger.js');
+
+
+module.exports = router;
