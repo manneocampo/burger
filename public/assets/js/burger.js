ce5ec191aafa5a4c7cb65179157519a06265f614
Michelle <manneocampo@gmail.com>
2017-12-06 22:38:09 -0500

    added some pseudocode for devour btn

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -10,5 +10,11 @@ $('#submit').click(function(e) {
 
 $('#devour').click(function(e){
 	e.preventDefault();
-	
-})
+	var devour = $('#devour').val();
+	console.log($('#devour'));
+	console.log("btn click");
+	if (devour){
+		return true; //need to be able to update the database with the form input burgerNames values for devour to true
+		//look into method-override to be able to use .put for form udpating and read more about .get and .post for forms
+	}
+});
